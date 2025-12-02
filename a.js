855ff3a9d6fe2f1064ddcaca066a815832c67d01
WeiJiaXing <weijiaxing765@gmail.com>
2025-12-02 19:20:18 +0800

    12-2

--- a.js
+++ a.js
@@ -1,15 +1,6 @@
 const arr = [1,1,2,4,4,5]
 
-const a1 = [...new Set(arr)]
-console.log(a1)
 
-const a2 = Array.from(new Set(arr))
-console.log(a2)
+const a5 = arr.find((x) => x = 4)
+console.log(a5)
 
-const map = new Map()
-const a3 = []
-for(let i = 0; i < arr.length; arr++){
-    if(!map.has(arr[i])){
-        map.set()
-    }
-}
