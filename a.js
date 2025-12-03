0de8e1a78fd80ce415b690a11815aa84d4af8a9a
WeiJiaXing <weijiaxing765@gmail.com>
2025-12-03 11:38:39 +0800

    12-3

--- a.js
+++ a.js
@@ -1,6 +1,18 @@
-const arr = [1,1,2,4,4,5]
+var threeSum = function (nums) {
+    nums.sort((a,b) => a -b)
+    const n = nums.length
+    const result = []
 
+    for(let i = 0; i < n - 2; i++){
+        if(i > 0 && nums[i] === nums[i - 1]) continue
+        if(nums[i] > 0) break
 
-const a5 = arr.find((x) => x = 4)
-console.log(a5)
+        let left = i + 1;
+        let right = n - 1;
 
+        while(left < right){
+            
+        }
+    }
+
+}
