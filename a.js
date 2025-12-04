140570b0901ec388025307e7f85ec027f801fd4e
WeiJiaXing <weijiaxing765@gmail.com>
2025-12-04 15:43:37 +0800

    12-4

--- a.js
+++ a.js
@@ -1,18 +1,14 @@
-var threeSum = function (nums) {
-    nums.sort((a,b) => a -b)
-    const n = nums.length
-    const result = []
+var threeSum = function (matrix) {
+    const n = matrix.length;
 
-    for(let i = 0; i < n - 2; i++){
-        if(i > 0 && nums[i] === nums[i - 1]) continue
-        if(nums[i] > 0) break
-
-        let left = i + 1;
-        let right = n - 1;
-
-        while(left < right){
-            
+    for(let i = 0; i < n; i++){
+        for(let j = i + 1; j < n; j++){
+            [matrix[i][j],matrix[j][i]] = []
         }
+    }
+
+    for(let i = 0; i < n; i++){
+        matrix[i].reverse()
     }
 
 }
